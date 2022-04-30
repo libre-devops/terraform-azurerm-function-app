5b415fb93a211511242dd088ce1e9186565328e6
Craig Thacker <craigthackerx@gmail.com>
2022-05-01 00:42:53 +0100

    Update module

--- fnc-app.tf
+++ fnc-app.tf
@@ -1,31 +1,36 @@
 resource "azurerm_function_app" "function_app" {
-  count = var.app_amount
-  name = "${var.app_name}${format("%02d", count.index + 1)}"
+  count                      = var.app_amount
+  name                       = "${var.app_name}${format("%02d", count.index + 1)}"
   app_service_plan_id        = var.app_service_plan_id
   location                   = var.location
   resource_group_name        = var.rg_name
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
   os_type                    = var.os_type
-  https_only = var.https_only
-
+  https_only                 = var.https_only
   app_settings = var.function_app_application_settings
 
   dynamic "site_config" {
-    for_each = [var.site_config]
+    for_each = lookup(var.settings, "site_config", {}) != {} ? [1] : []
+
     content {
-      always_on                   = lookup(site_config.value, "always_on", null)
-      ftps_state                  = lookup(site_config.value, "ftps_state", null)
-      http2_enabled               = lookup(site_config.value, "http2_enabled", null)
-      ip_restriction              = lookup(site_config.value, "ip_restriction", null)
-      linux_fx_version            = lookup(site_config.value, "linux_fx_version", null)
-      min_tls_version             = lookup(site_config.value, "min_tls_version", null)
-      pre_warmed_instance_count   = lookup(site_config.value, "pre_warmed_instance_count", null)
-      scm_use_main_ip_restriction = var.scm_authorized_ips != [] || var.scm_authorized_subnet_ids != null || lookup(site_config.value, "scm_ip_restriction", null) != null ? false : true
-      scm_ip_restriction          = concat(var.scm_subnets, var.scm_cidrs, var.scm_service_tags, lookup(site_config.value, "scm_ip_restriction", []))
-      scm_type                    = lookup(site_config.value, "scm_type", null)
-      use_32_bit_worker_process   = lookup(site_config.value, "use_32_bit_worker_process", null)
-      websockets_enabled          = lookup(site_config.value, "websockets_enabled", null)
+      always_on                        = lookup(var.settings.site_config, "always_on", false)
+      app_scale_limit                  = lookup(var.settings.site_config, "app_scale_limit", null)
+      elastic_instance_minimum         = lookup(var.settings.site_config, "elastic_instance_minimum", null)
+      health_check_path                = lookup(var.settings.site_config, "health_check_path", null)
+      min_tls_version                  = lookup(var.settings.site_config, "min_tls_version", null)
+      pre_warmed_instance_count        = lookup(var.settings.site_config, "pre_warmed_instance_count", null)
+      runtime_scale_monitoring_enabled = lookup(var.settings.site_config, "runtime_scale_monitoring_enabled", null)
+      dotnet_framework_version         = lookup(var.settings.site_config, "dotnet_framework_version", null)
+      ftps_state                       = lookup(var.settings.site_config, "ftps_state", null)
+      http2_enabled                    = lookup(var.settings.site_config, "http2_enabled", null)
+      java_version                     = lookup(var.settings.site_config, "java_version", null)
+      linux_fx_version                 = lookup(var.settings.site_config, "linux_fx_version", null)
+      use_32_bit_worker_process        = lookup(var.settings.site_config, "use_32_bit_worker_process", null)
+      websockets_enabled               = lookup(var.settings.site_config, "websockets_enabled", null)
+      scm_type                         = lookup(var.settings.site_config, "scm_type", null)
+      scm_use_main_ip_restriction      = lookup(var.settings.site_config, "scm_use_main_ip_restriction", null)
+      vnet_route_all_enabled           = lookup(var.settings.site_config, "vnet_route_all_enabled", null)
 
       dynamic "cors" {
         for_each = lookup(site_config.value, "cors", []) != [] ? ["fake"] : []
@@ -34,38 +39,101 @@ resource "azurerm_function_app" "function_app" {
           support_credentials = lookup(site_config.value.cors, "support_credentials", false)
         }
       }
-    }
-  }
 
-   auth_settings {
-    enabled                        = var.enable_auth_settings
-    default_provider               = var.default_auth_provider
-    allowed_external_redirect_urls = []
-    issuer                         = format("https://sts.windows.net/%s/", data.azurerm_client_config.main.tenant_id)
-    unauthenticated_client_action  = var.unauthenticated_client_action
-    token_store_enabled            = var.token_store_enabled
+       dynamic "ip_restriction" {
+        for_each = try(var.settings.site_config.ip_restriction, {})
 
-    dynamic "active_directory" {
-      for_each = var.active_directory_auth_setttings
-      content {
-        client_id         = lookup(active_directory_auth_setttings.value, "client_id", null)
-        client_secret     = lookup(active_directory_auth_setttings.value, "client_secret", null)
-        allowed_audiences = concat(formatlist("https://%s", [format("%s.azurewebsites.net", var.app_service_name)]), [])
+        content {
+          ip_address                = lookup(ip_restriction, "ip_address", null)
+          virtual_network_subnet_id = lookup(ip_restriction, "virtual_network_subnet_id", null)
+        }
+      }
+      dynamic "scm_ip_restriction" {
+        for_each = try(var.settings.site_config.scm_ip_restriction, {})
+
+        content {
+          ip_address                = lookup(scm_ip_restriction, "ip_address", null)
+          service_tag               = lookup(scm_ip_restriction, "service_tag", null)
+          virtual_network_subnet_id = lookup(scm_ip_restriction, "virtual_network_subnet_id", null)
+          name                      = lookup(scm_ip_restriction, "name", null)
+          priority                  = lookup(scm_ip_restriction, "priority", null)
+          action                    = lookup(scm_ip_restriction, "action", null)
+          dynamic "headers" {
+            for_each = try(scm_ip_restriction.headers, {})
+
+            content {
+              x_azure_fdid      = lookup(headers, "x_azure_fdid", null)
+              x_fd_health_probe = lookup(headers, "x_fd_health_probe", null)
+              x_forwarded_for   = lookup(headers, "x_forwarded_for", null)
+              x_forwarded_host  = lookup(headers, "x_forwarded_host", null)
+            }
+          }
+        }
       }
     }
   }
 
-  dynamic "backup" {
-    for_each = var.enable_backup ? [{}] : []
+   dynamic "auth_settings" {
+    for_each = lookup(var.settings, "auth_settings", {}) != {} ? [1] : []
+
     content {
-      name                = coalesce(var.backup_settings.name, "DefaultBackup")
-      enabled             = var.backup_settings.enabled
-      storage_account_url = format("https://${data.azurerm_storage_account.storeacc.0.name}.blob.core.windows.net/${azurerm_storage_container.storcont.0.name}%s", data.azurerm_storage_account_blob_container_sas.main.0.sas)
-      schedule {
-        frequency_interval       = var.backup_settings.frequency_interval
-        frequency_unit           = var.backup_settings.frequency_unit
-        retention_period_in_days = var.backup_settings.retention_period_in_days
-        start_time               = var.backup_settings.start_time
+      enabled                        = lookup(var.settings.auth_settings, "enabled", false)
+      additional_login_params        = lookup(var.settings.auth_settings, "additional_login_params", null)
+      allowed_external_redirect_urls = lookup(var.settings.auth_settings, "allowed_external_redirect_urls", null)
+      default_provider               = lookup(var.settings.auth_settings, "default_provider", null)
+      issuer                         = lookup(var.settings.auth_settings, "issuer", null)
+      runtime_version                = lookup(var.settings.auth_settings, "runtime_version", null)
+      token_refresh_extension_hours  = lookup(var.settings.auth_settings, "token_refresh_extension_hours", null)
+      token_store_enabled            = lookup(var.settings.auth_settings, "token_store_enabled", null)
+      unauthenticated_client_action  = lookup(var.settings.auth_settings, "unauthenticated_client_action", null)
+
+      dynamic "active_directory" {
+        for_each = lookup(var.settings.auth_settings, "active_directory", {}) != {} ? [1] : []
+
+        content {
+          client_id         = var.settings.auth_settings.active_directory.client_id
+          client_secret     = lookup(var.settings.auth_settings.active_directory, "client_secret", null)
+          allowed_audiences = lookup(var.settings.auth_settings.active_directory, "allowed_audiences", null)
+        }
+      }
+
+      dynamic "facebook" {
+        for_each = lookup(var.settings.auth_settings, "facebook", {}) != {} ? [1] : []
+
+        content {
+          app_id       = var.settings.auth_settings.facebook.app_id
+          app_secret   = var.settings.auth_settings.facebook.app_secret
+          oauth_scopes = lookup(var.settings.auth_settings.facebook, "oauth_scopes", null)
+        }
+      }
+
+      dynamic "google" {
+        for_each = lookup(var.settings.auth_settings, "google", {}) != {} ? [1] : []
+
+        content {
+          client_id     = var.settings.auth_settings.google.client_id
+          client_secret = var.settings.auth_settings.google.client_secret
+          oauth_scopes  = lookup(var.settings.auth_settings.google, "oauth_scopes", null)
+        }
+      }
+
+      dynamic "microsoft" {
+        for_each = lookup(var.settings.auth_settings, "microsoft", {}) != {} ? [1] : []
+
+        content {
+          client_id     = var.settings.auth_settings.microsoft.client_id
+          client_secret = var.settings.auth_settings.microsoft.client_secret
+          oauth_scopes  = lookup(var.settings.auth_settings.microsoft, "oauth_scopes", null)
+        }
+      }
+
+      dynamic "twitter" {
+        for_each = lookup(var.settings.auth_settings, "twitter", {}) != {} ? [1] : []
+
+        content {
+          consumer_key    = var.settings.auth_settings.twitter.consumer_key
+          consumer_secret = var.settings.auth_settings.twitter.consumer_secret
+        }
       }
     }
   }
@@ -79,11 +147,6 @@ resource "azurerm_function_app" "function_app" {
     }
   }
 
-  identity {
-    type         = var.identity_ids != null ? "SystemAssigned, UserAssigned" : "SystemAssigned"
-    identity_ids = var.identity_ids
-  }
-
   dynamic "storage_account" {
     for_each = var.storage_mounts
     content {
@@ -93,6 +156,18 @@ resource "azurerm_function_app" "function_app" {
       share_name   = lookup(storage_account.value, "share_name", null)
       access_key   = lookup(storage_account.value, "access_key", null)
       mount_path   = lookup(storage_account.value, "mount_path", null)
+    }
+  }
+
+  dynamic "source_control" {
+    for_each = lookup(var.settings, "source_control", {}) != {} ? [1] : []
+
+    content {
+      repo_url           = var.settings.source_control.repo_url
+      branch             = lookup(var.settings.source_control, "branch", null)
+      manual_integration = lookup(var.settings.source_control, "manual_integration", null)
+      rollback_enabled   = lookup(var.settings.source_control, "rollback_enabled", null)
+      use_mercurial      = lookup(var.settings.source_control, "use_mercurial", null)
     }
   }
 
@@ -121,7 +196,7 @@ resource "azurerm_function_app" "function_app" {
     }
   }
 
-  version = "~${var.function_app_version}"
+  version = var.function_app_version
 
   tags = var.tags
 }
--- input.tf
+++ input.tf
@@ -1,111 +1,22 @@
-variable "app_name" {
-  description = "The name of the function app"
-  type = string
+variable "active_directory_auth_setttings" {
+  description = "Acitve directory authentication provider settings for app service"
+  type        = any
+  default     = {}
 }
 
 variable "app_amount" {
   description = "The amount of apps to be created"
-  type = number
-}
-
-variable "environment" {
-  description = "Project environment"
-  type        = string
-}
-
-variable "stack" {
-  description = "Project stack name"
-  type        = string
-}
-
-variable "storage_account_name" {
-  description = "Name of storage account"
-  type = string
-}
-
-variable "tags" {
-  type        = map(string)
-  description = "A map of the tags to use on the resources that are deployed with this module."
-  default = {
-    source = "terraform"
-  }
-}
-
-variable "rg_name" {
-  description = "Resource group name"
-  type        = string
-}
-
-variable "location" {
-  description = "Azure location."
-  type        = string
-}
-
-variable "location_short" {
-  description = "Short string for Azure location."
-  type        = string
-}
-
-variable "function_app_version" {
-  description = "Version of the function app runtime to use (Allowed values 2 or 3)"
   type        = number
-  default     = 3
 }
 
-variable "storage_account_access_key" {
-  description = "Access key the storage account to use. If null a new storage account is created"
+variable "app_name" {
+  description = "The name of the function app"
   type        = string
-  default     = null
-}
-
-variable "scm_subnets" {
-  description = "Subnets for SCM"
-  type = list(string)
-}
-
-variable "scm_cidrs" {
-  description = "CIDRs for SCM"
-  type = list(string)
-}
-
-variable "scm_service_tags" {
-  description = "Service tags for SCM"
-  type = list(string)
-}
-
-variable "storage_account_kind" {
-  description = "Storage Account Kind"
-  type        = string
-  default     = "StorageV2"
-}
-
-variable "storage_account_min_tls_version" {
-  description = "Storage Account minimal TLS version"
-  type        = string
-  default     = "TLS1_2"
-}
-
-variable "storage_account_enable_advanced_threat_protection" {
-  description = "Boolean flag which controls if advanced threat protection is enabled, see [here](https://docs.microsoft.com/en-us/azure/storage/common/storage-advanced-threat-protection?tabs=azure-portal) for more information."
-  type        = bool
-  default     = false
-}
-
-variable "storage_account_enable_https_traffic_only" {
-  description = "Boolean flag which controls if https traffic only is enabled."
-  type        = bool
-  default     = true
 }
 
 variable "app_service_plan_id" {
   description = "Id of the App Service Plan for Function App hosting"
   type        = string
-}
-
-variable "function_language_for_linux" {
-  description = "Language of the Function App on Linux hosting, can be \"dotnet\", \"node\" or \"python\""
-  type        = string
-  default     = "dotnet"
 }
 
 variable "application_insights_enabled" {
@@ -126,26 +37,8 @@ variable "application_insights_type" {
   default     = "web"
 }
 
-variable "function_app_application_settings" {
-  description = "Function App application settings"
-  type        = map(string)
-  default     = {}
-}
-
-variable "identity_type" {
-  description = "Add an Identity (MSI) to the function app. Possible values are SystemAssigned or UserAssigned"
-  type        = string
-  default     = "SystemAssigned"
-}
-
-variable "identity_ids" {
-  description = "UserAssigned Identities ID to add to Function App. Mandatory if type is UserAssigned"
-  type        = list(string)
-  default     = null
-}
-
-variable "os_type" {
-  description = "A string indicating the Operating System type for this function app."
+variable "application_zip_package_path" {
+  description = "Local or remote path of a zip package to deploy on the Function App"
   type        = string
   default     = null
 }
@@ -162,10 +55,78 @@ variable "authorized_subnet_ids" {
   default     = []
 }
 
-variable "ip_restriction_headers" {
-  description = "IPs restriction headers for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#headers"
-  type        = map(list(string))
+variable "backup_settings" {
+  description = "Backup settings for App service"
+  type = object({
+    name                     = string
+    enabled                  = bool
+    storage_account_url      = optional(string)
+    frequency_interval       = number
+    frequency_unit           = optional(string)
+    retention_period_in_days = optional(number)
+    start_time               = optional(string)
+  })
+  default = {
+    enabled                  = false
+    name                     = "DefaultBackup"
+    frequency_interval       = 1
+    frequency_unit           = "Day"
+    retention_period_in_days = 1
+  }
+}
+
+variable "connection_strings" {
+  description = "Connection strings for App Service"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "custom_domains" {
+  description = "Custom domains with SSL binding and SSL certificates for the App Service. Getting the SSL certificate from an Azure Keyvault Certificate Secret or a file is possible."
+  type        = map(map(string))
   default     = null
+}
+
+variable "default_auth_provider" {
+  description = "The default provider to use when multiple providers have been set up. Possible values are `AzureActiveDirectory`, `Facebook`, `Google`, `MicrosoftAccount` and `Twitter`"
+  default     = "AzureActiveDirectory"
+}
+
+variable "disable_ip_masking" {
+  description = "By default the real client ip is masked as `0.0.0.0` in the logs. Use this argument to disable masking and log the real client ip"
+  default     = false
+}
+
+variable "enable_backup" {
+  description = "bool to to setup backup for app service "
+  default     = false
+}
+
+variable "enable_client_affinity" {
+  description = "Should the App Service send session affinity cookies, which route client requests in the same session to the same instance?"
+  default     = false
+}
+
+variable "enable_client_certificate" {
+  description = "Does the App Service require client certificates for incoming requests"
+  default     = false
+}
+
+variable "enable_https" {
+  description = "Can the App Service only be accessed via HTTPS?"
+  default     = false
+}
+
+variable "function_app_application_settings" {
+  description = "Function App application settings"
+  type        = map(string)
+  default     = {}
+}
+
+variable "function_app_version" {
+  description = "Version of the function app runtime to use (Allowed values 2 or 3)"
+  type        = string
+  default     = "~3"
 }
 
 variable "function_app_vnet_integration_enabled" {
@@ -181,6 +142,67 @@ variable "function_app_vnet_integration_subnet_id" {
 }
 
 # SCM parameters
+
+variable "function_language_for_linux" {
+  description = "Language of the Function App on Linux hosting, can be \"dotnet\", \"node\" or \"python\""
+  type        = string
+  default     = "dotnet"
+}
+
+variable "https_only" {
+  description = "Disable http procotol and keep only https"
+  type        = bool
+  default     = true
+}
+
+variable "identity_ids" {
+  description = "UserAssigned Identities ID to add to Function App. Mandatory if type is UserAssigned"
+  type        = list(string)
+  default     = null
+}
+
+variable "identity_type" {
+  description = "Add an Identity (MSI) to the function app. Possible values are SystemAssigned or UserAssigned"
+  type        = string
+  default     = "SystemAssigned"
+}
+
+variable "ip_restriction_headers" {
+  description = "IPs restriction headers for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#headers"
+  type        = map(list(string))
+  default     = null
+}
+
+variable "location" {
+  description = "Azure location."
+  type        = string
+}
+
+variable "os_type" {
+  description = "A string indicating the Operating System type for this function app."
+  type        = string
+  default     = null
+}
+
+variable "password_end_date" {
+  description = "The relative duration or RFC3339 rotation timestamp after which the password expire"
+  default     = null
+}
+
+variable "password_rotation_in_years" {
+  description = "Number of years to add to the base timestamp to configure the password rotation timestamp. Conflicts with password_end_date and either one is specified and not the both"
+  default     = 2
+}
+
+variable "retention_in_days" {
+  description = "Specifies the retention period in days. Possible values are `30`, `60`, `90`, `120`, `180`, `270`, `365`, `550` or `730`"
+  default     = 90
+}
+
+variable "rg_name" {
+  description = "Resource group name"
+  type        = string
+}
 
 variable "scm_authorized_ips" {
   description = "SCM IPs restriction for Function. See documentation https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/function_app#scm_ip_restriction"
@@ -200,20 +222,58 @@ variable "scm_ip_restriction_headers" {
   default     = null
 }
 
+variable "scm_subnets" {
+  description = "Subnets for SCM"
+  type        = list(string)
+}
+
+variable "settings" {
+  description = "Specifies the Authenication enabled or not"
+  default     = false
+}
+
 variable "site_config" {
   description = "Site config for App Service. See documentation https://www.terraform.io/docs/providers/azurerm/r/app_service.html#site_config. IP restriction attribute is not managed in this block."
   type        = any
   default     = {}
 }
 
-variable "https_only" {
-  description = "Disable http procotol and keep only https"
-  type        = bool
-  default     = true
-}
-
-variable "application_zip_package_path" {
-  description = "Local or remote path of a zip package to deploy on the Function App"
+variable "storage_account_access_key" {
+  description = "Access key the storage account to use. If null a new storage account is created"
   type        = string
   default     = null
+}
+
+variable "storage_account_name" {
+  description = "Name of storage account"
+  type        = string
+}
+
+variable "storage_container_name" {
+  description = "The name of the storage container to keep backups"
+  default     = null
+}
+
+variable "storage_mounts" {
+  description = "Storage account mount points for App Service"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module."
+  default = {
+    source = "terraform"
+  }
+}
+
+variable "token_store_enabled" {
+  description = "If enabled the module will durably store platform-specific security tokens that are obtained during login flows"
+  default     = false
+}
+
+variable "unauthenticated_client_action" {
+  description = "The action to take when an unauthenticated client attempts to access the app. Possible values are `AllowAnonymous` and `RedirectToLoginPage`"
+  default     = "RedirectToLoginPage"
 }
